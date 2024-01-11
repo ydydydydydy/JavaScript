9e2931b5a145a80438275fe06d9b655d17ee2b89
unknown <ekdud6887@gmail.com>
2024-01-11 17:49:19 +0900

    studyed

--- API/ex02movieFetch.js
+++ API/ex02movieFetch.js
@@ -1,13 +1,29 @@
-let movieUrl = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101';
 
 document.querySelector('button')
 .addEventListener('click', ()=>{
+    let date = prompt('날짜를 입력하세요.', 'YYYYMMDD')
+    let movieUrl = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=${date}`;
     console.log('fetch click');
     fetch(movieUrl)
     .then(res=>res.json())
-    .then((data)=>{
+    .then((result)=>{
         // 성공
-        console.log(data);
+        console.log(result);
+        let movieList = result.boxOfficeResult.dailyBoxOfficeList
+        let table = '';
+        table += `<table><tr><th>순위</th><th>영화명</th><th>개봉일</th></tr>`
+        movieList.forEach(item=>{
+            table += `
+            <tr>
+                <td>${item.rank}</td>
+                <td>${item.movieNm}</td>
+                <td>${item.openDt}</td>
+            </tr>
+            `
+        })
+        table += `</table>`
+
+        document.querySelector('div').innerHTML = table;
     })
-    .catch(error=>console.log(error))
+    .catch(error=>console.error(error))
 })
